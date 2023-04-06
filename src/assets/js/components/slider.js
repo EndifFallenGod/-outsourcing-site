6fe769d3b3a3aace67d6eaf1ea6f934f96603026
Maxim <maksik-maksik-2001@mail.ru>
2023-04-06 23:56:28 +0300

    section reviews, slider added

---
+++ src/assets/js/components/slider.js
@@ -0,0 +1,10 @@
+// import $ from "jquery";
+// import "slick-carousel";
+
+$("[data-slider]").slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  fade: false,
+});
